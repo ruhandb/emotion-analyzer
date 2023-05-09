9c998125b8df47faace4372fddfb81816093d5f8
Ruhan Borgmann <ruhandb@gmail.com>
2023-05-09 14:47:54 +0100

    port

--- src/index.js
+++ src/index.js
@@ -56,5 +56,5 @@ app.post('/analyzeSentiment', (req, res) => {
 // Start server
 let port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log('Server started on port 8080');
+  console.log('Server started on port ' + port);
 });
